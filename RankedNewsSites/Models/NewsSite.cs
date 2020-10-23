1fc9778c0f8fdb1f553df01b6d949f0a8ee7d583
Axmart <notaxmart@gmail.com>
2020-10-23 22:54:07 +0200

    Add project files.

---
+++ RankedNewsSites/Models/NewsSite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RankedNewsSites.Models
+{
+    public class NewsSite
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string PictureSource { get; set; }
+        public int Points { get; set; }
+    }
+}
