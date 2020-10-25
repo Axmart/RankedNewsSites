437c39ad274b33b89d24058bfe2fddf46f672454
Axmart <notaxmart@gmail.com>
2020-10-25 13:09:09 +0100

    Creating roles listroles

--- RankedNewsSites/Controllers/AdministrationController.cs
+++ RankedNewsSites/Controllers/AdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using RankedNewsSites.Models;
 using RankedNewsSites.ViewModel;
 using System;
@@ -12,10 +13,12 @@ namespace RankedNewsSites.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -40,7 +43,7 @@ namespace RankedNewsSites.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Browse", "NewsSites");
+                    return RedirectToAction("ListRoles");
                 }
 
                 foreach (var error in result.Errors)
@@ -52,6 +55,80 @@ namespace RankedNewsSites.Controllers
 
             return View(model);
         }
+
+
+        public IActionResult ListRoles()
+        {
+            var roles = roleManager.Roles;
+
+            return View(roles);
+
+        }
+
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewData["ErrorMessage"] = $"Role with ID = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+
+
+            };
+
+            foreach (var user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.users.Add(user.UserName);
+                }
+                
+            }
+
+            return View(model);
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewData["ErrorMessage"] = $"Role with ID = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+
+
+            return View(model);
+
+        }
+
+
 
 
     }
---
+++ RankedNewsSites/ViewModel/EditRoleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RankedNewsSites.ViewModel
+{
+    public class EditRoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage ="Role Name is required")]
+        public string RoleName { get; set; }
+
+        public List<string> users { get; set; }
+
+
+        public EditRoleViewModel()
+        {
+            users = new List<string>();
+        }
+    }
+}
